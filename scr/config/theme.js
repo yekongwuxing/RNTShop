18b3d0167d345884001088739cb5966d36b3fa1c
jiangmm <jiangmm@jchvip.com>
2018-08-01 15:00:36 +0800

    适配iPhone X

--- scr/config/theme.js
+++ scr/config/theme.js
@@ -10,10 +10,20 @@ export default module = {
         height: Platform.OS === 'Andriod' ? 56 : 44,
         backgroundColor: '#fff'
     },
-    barContentPad: (Platform.OS === 'android' ? 0 : 20), //根据不同平台顶部 padding不一致
+    barContentPad: (Platform.OS === 'android' ? 0 : (isIponeX() ? 42 : 20)), //根据不同平台顶部 padding不一致
+    bottomPadding:isIponeX() ? 18 : 0,
     primaryColor: '#EE0000',
     lightGray: '#f5f5f5',
     lightBlack: '#333333',
 
-
 };
+export function isIponeX() {
+    let dimension = Dimensions.get('window');
+    return(
+        Platform.OS === 'ios' &&
+        !Platform.isPad &&
+        !Platform.isTVOS &&
+        (dimension.height === 812 || dimension.width === 812)
+    );
+
+}
