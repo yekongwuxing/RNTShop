f2c8cf77407f3cb6b1db3cd30489db23a4f108ce
jiangmm <jiangmm@jchvip.com>
2018-08-02 09:08:26 +0800

    适配安卓，没有运行调试

--- scr/pages/CategoryPage.js
+++ scr/pages/CategoryPage.js
@@ -2,14 +2,27 @@ import React, {Component} from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    StatusBar,
+    Platform
 } from 'react-native';
 
 export default class IndexPage extends Component {
 
+    componentWillMount() {
+        this._navListener.remove();
+    }
+
+    componentDidMount() {
+        this._navListener = this.props.navigation.addListener('didFocus',() => {
+            StatusBar.setState('dark-content');
+            Platform.OS === 'andriod' && StatusBar.setBackgroundColor('#fff');
+        });
+    }
     render() {
         return ( // 渲染布局
             <View style={styles.container}>
+                <StatusBar barStyle={'dark-content'} backgroundColor={'#fff'}/>
                 <Text style={styles.txt}>分类</Text>
             </View>
         );
