762500702819e81f368ffa02754a5590eef3776b
jiangmm <jiangmm@jchvip.com>
2018-08-03 16:08:33 +0800

    品类界面

--- scr/pages/CategoryPage.js
+++ scr/pages/CategoryPage.js
@@ -2,31 +2,62 @@ import React, {Component} from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity,
+    FlatList,
+    Platform,
+    StatusBar,
+    Image
 } from 'react-native';
+import theme from '../config/theme';
+import px2dp from '../utils/px2dp';
+
+let imageResource = [require('../images/category1.png'),require('../images/category2.png'),
+    require('../images/category3.png'),require('../images/category4.png')];
 
 export default class IndexPage extends Component {
+    componentWillUnmount() {
+        this._navListener.remove();
+    }
 
+    componentDidMount() {
+        this._navListener = this.props.navigation.addListener('didFocus', () => {
+            StatusBar.setBarStyle('dark-content');
+            Platform.OS === 'android' && StatusBar.setBackgroundColor('#fff');
+        });
+    }
     render() {
         return ( // 渲染布局
             <View style={styles.container}>
-                <Text style={styles.txt}>分类</Text>
+                <FlatList style={{flex:1}}
+                data={imageResource}
+                renderItem={this._renderItemComponent}
+                keyExtractor={this._keyExtractor}/>
             </View>
         );
+    }
+    _keyExtractor = (item,index) => 'Category' + index;
+    _renderItemComponent = ({item,index}) => {
+        return (
+            <View style={{backgroundColor:'#fff',paddingVertical:px2dp(10),alignItems:'center'}}>
+                <TouchableOpacity onPress={this._onItemPress.bind(this,index)}>
+                    <Image source={item}/>
+                </TouchableOpacity>
+            </View>
+
+        );
+    }
+    //条目点击事件
+    _onItemPress(index) {
+        console.log(index)
+
     }
 }
 // 样式文件
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    txt:{
-        margin:10,
-        fontSize:20,
-        color:'black'
-
+        backgroundColor:theme.lightGray,
+        paddingTop:px2dp(1)
     }
 });
