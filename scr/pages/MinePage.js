d26f98d66438915a3d021ad39ed3013e955da9bd
jiangmm <jiangmm@jchvip.com>
2018-08-03 15:27:24 +0800

    我的界面下半部分搭建

--- scr/pages/MinePage.js
+++ scr/pages/MinePage.js
@@ -7,13 +7,14 @@ import {
     TouchableOpacity,
     ImageBackground,
     StatusBar,
-    Platform
+    Platform,
+    ScrollView
 
 } from 'react-native';
 import theme from '../config/theme';
 import TextFix from '../component/TextFix'
 import px2dp from '../utils/px2dp'
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
 export default class IndexPage extends Component {
 
@@ -21,8 +22,11 @@ export default class IndexPage extends Component {
       constructor(props) {
         super(props);
         // 初始状态
-        this.state = {};
+        this.state = {
+            data:''
+        };
       }
+
     render() {
         return ( // 渲染布局
             <View style={styles.container}>
@@ -30,7 +34,7 @@ export default class IndexPage extends Component {
                                  style={styles.imgTabBar}>
                     <View style={{flexDirection:'row-reverse'}}>
                         <TouchableOpacity style={{paddingRight:px2dp(20),paddingTop:px2dp(16)}}>
-                            <Ionicons name={'ios-settings'} size={28} color={'#666'}/>
+                            <SimpleLineIcons name={'settings'} size={20} color={'#666'}/>
                         </TouchableOpacity>
                     </View>
                     <View style={{flexDirection:'row',flex:1,alignItems:'center', paddingHorizontal:px2dp(24)}}>
@@ -58,7 +62,53 @@ export default class IndexPage extends Component {
                         </View>
                     </View>
                 </ImageBackground>
-
+                <ScrollView>
+                    <View style={[styles.rowItem,{height:px2dp(100), marginTop:px2dp(32)}]}>
+                        <TextFix style={{fontSize:px2dp(30),color:'#333333',flex:1}}>购买的订单</TextFix>
+                        <TouchableOpacity style={{flexDirection:'row',alignItems:'center'} }>
+                            <TextFix style={{fontSize:px2dp(24),color:'#999999', marginRight:px2dp(24)}}>购买的订单</TextFix>
+                            <SimpleLineIcons name={'arrow-right'} size={16} color={'#999999'}/>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={[styles.rowItem,{height:px2dp(140)}]}>
+                        <TouchableOpacity style={styles.countItem}>
+                            <Image source={require('../images/to_pay.png')}/>
+                            <TextFix style={styles.buttonTxt}>待付款</TextFix>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.countItem}>
+                            <Image source={require('../images/to_ship.png')}/>
+                            <TextFix style={styles.buttonTxt}>待发货</TextFix>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.countItem}>
+                            <Image source={require('../images/to_receive.png')}/>
+                            <TextFix style={styles.buttonTxt}>待收货</TextFix>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.countItem}>
+                            <Image source={require('../images/to_finish.png')}/>
+                            <TextFix style={styles.buttonTxt}>未评价</TextFix>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.countItem}>
+                            <Image source={require('../images/to_issue.png')}/>
+                            <TextFix style={styles.buttonTxt}>退换货</TextFix>
+                        </TouchableOpacity>
+                    </View>
+                    <TouchableOpacity style={[styles.rowItem,styles.borderTop,{height:px2dp(100)}]}>
+                        <TextFix style={styles.itemTxt}>我的地址</TextFix>
+                        <SimpleLineIcons name={'arrow-right'} size={20} color={'#999999'}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.rowItem,styles.borderTop,{height:px2dp(100)}]}>
+                        <TextFix style={styles.itemTxt}>账户与安全</TextFix>
+                        <SimpleLineIcons name={'arrow-right'} size={20} color={'#999999'}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.rowItem,styles.borderTop,{height:px2dp(100)}]}>
+                        <TextFix style={styles.itemTxt}>客服与帮助</TextFix>
+                        <SimpleLineIcons name={'arrow-right'} size={20} color={'#999999'}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.rowItem,styles.borderTop,{height:px2dp(100)}]}>
+                        <TextFix style={styles.itemTxt}>意见反馈</TextFix>
+                        <SimpleLineIcons name={'arrow-right'} size={20} color={'#999999'}/>
+                    </TouchableOpacity>
+                </ScrollView>
             </View>
         );
     }
@@ -98,5 +148,25 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         alignItems:'center',
         flex:1
+    },
+    rowItem: {
+        marginHorizontal: px2dp(24),
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    buttonTxt:{
+        fontSize:px2dp(24),
+        color:'#333333',
+        marginTop:px2dp(12)
+    },
+    itemTxt:{
+        fontSize:px2dp(32),
+        color:'#333333',
+        flex:1
+    },
+    borderTop:{
+        borderTopColor:'#f5f5f5',
+        borderTopWidth:px2dp(1)
     }
+
 });
